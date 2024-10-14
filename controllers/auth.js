8486ed3e2fe0e7cbd48fe076bc00316a03372513
Ángel Illescas Cruz <angelalfonso.illescas@gmail.com>
2024-10-14 17:50:49 -0600

    feat(middleware): Improve error handling and validation consistency with custom middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,9 @@
 const express = require('express')
-const { validationResult } = require('express-validator')
 
 const createUser = (req, res = express.response) => {
 
     const { name, password, email } = req.body
-
-    // Manejo de errores
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
+    
     res.status(201).json({
         ok: true,
         msg: "Register",
@@ -25,15 +16,6 @@ const createUser = (req, res = express.response) => {
 const loginUser = (req, res = express.response) => {
 
     const { password, email } = req.body
-
-    // Manejo de errores
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
 
     res.status(201).json({
         ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -7,18 +7,21 @@ const { Router } = require('express')
 const router = Router()
 const { check } = require('express-validator')
 const { createUser, loginUser, renewToken } = require('../controllers/auth')
+const {fieldValidator} = require('../middlewares/fieldValidator')
 
 router.post('/new', [
     //coleccion de midleware para validacion
     check('name', 'Nombre obligatorio').not().isEmpty(),
     check('email', 'Email obligatorio').isEmail(),
-    check('password', 'Password debe de ser de 6 caracteres').isLength({ min: 6 })
+    check('password', 'Password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    fieldValidator
 ], createUser)
 
 router.post('/', [
     //coleccion de midleware para validacion
     check('email', 'Email obligatorio').isEmail(),
-    check('password', 'Password debe de ser de 6 caracteres').isLength({ min: 6 })
+    check('password', 'Password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    fieldValidator
 ], loginUser)
 
 router.get('/renew', renewToken)
