d05e559197494a71815e6ac78c2d1991b5dce35e
Ángel Illescas Cruz <angelalfonso.illescas@gmail.com>
2024-10-14 12:51:49 -0600

    feat(backend): Add Express as dependency and initialize basic server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hola mundo");
+const express = require('express')
+
+const app = express()
+
+//Routes
+
+app.get('/',(req,res) => { 
+    res.json({
+        ok:true
+    })
+})
+
+//Listener
+
+app.listen(4000,() => { 
+    console.log(`Servidor corriendo en puerto ${4000}`);
+ })
