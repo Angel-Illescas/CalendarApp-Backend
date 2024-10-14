061692c701e207bc20531c29f5fb5a7e5ef238c7
Ángel Illescas Cruz <angelalfonso.illescas@gmail.com>
2024-10-14 13:57:35 -0600

    refactor(express/routes): Reorganize routes with auth controller for better readability

---
+++ controllers/auth.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const createUser = (req, res = express.response) => {
+    res.json({
+        ok: true,
+        msg: "Register",
+    })
+}
+
+const loginUser = (req, res = express.response) => {
+    res.json({
+        ok: true,
+        msg: "login",
+    })
+}
+
+const renewToken = (req, res = express.response) => {
+    res.json({
+        ok: true,
+        msg: "renew",
+    })
+}
+
+
+module.exports = {
+    createUser, loginUser, renewToken,
+}
