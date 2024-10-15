d7806c3ebbaf55cf4e26b88397dff7826cc4e233
Ángel Illescas Cruz <angelalfonso.illescas@gmail.com>
2024-10-15 01:57:00 -0600

    feat(security): Implement password hashing and JSON Web Tokens for authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,41 @@
 const express = require('express')
 const User = require('../models/User')
-
+const bcrypt = require('bcryptjs')
+const { generarJWT } = require('../helpers/jwt')
 
 
 const createUser = async (req, res = express.response) => {
 
-    const { name, password, email } = req.body
+    const { password, email } = req.body
 
     try {
-        const user = new User(req.body)
+
+        let user = await User.findOne({email})
+
+        if (user){
+            return res.status(400).json({
+                ok: false,
+                msg: "El usuario ya existe con el correo"
+            })
+        }
+
+         user = new User(req.body)
+
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt)
+
+        //Generar JWT
+        const token = await generarJWT( user.id, user.name )
+
         await user.save()
 
         res.status(201).json({
             ok: true,
             msg: "Register",
-            name,
-            password,
-            email
+            uid: user.id,
+            name: user.name,
+            token
         })
     } catch (error) {
         console.log(error);
@@ -28,16 +47,53 @@ const createUser = async (req, res = express.response) => {
     }
 }
 
-const loginUser = (req, res = express.response) => {
+const loginUser = async (req, res = express.response) => {
 
     const { password, email } = req.body
 
-    res.status(201).json({
-        ok: true,
-        msg: "login",
-        email,
-        password,
-    })
+    try {
+        let user = await User.findOne({email})
+
+        if (!user){
+            return res.status(400).json({
+                ok: false,
+                msg: "El usuario es incorrecto; contraseña o email incorrecto"
+            })
+        }
+
+        //Confirmar password
+
+        const validPassword = bcrypt.compareSync(password, user.password)
+
+        if (!validPassword){
+            return res.status(400).json({
+                ok:false,
+                msg:"Password incorrecto"
+            })
+        }
+
+        // Generar JWT
+
+        const token = await generarJWT( user.id, user.name )
+
+
+
+        res.json({
+            ok:true,
+            uid:user.id,
+            name:user.name,
+            token
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el administrador"
+        })
+    }
+
 }
 
 const renewToken = (req, res = express.response) => {
